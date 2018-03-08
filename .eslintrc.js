fb0e734c5e6bffcb18aa0df4ab20cdd1422379a4
staskorz <staskorz@gmail.com>
2018-03-08 13:36:48 +0200

    infra: Initial ESLint integration

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	root: true,
+
+	env: {
+		es6: true,
+	},
+
+	parserOptions: {
+		ecmaVersion: 2017,
+		sourceType: 'module',
+	},
+
+	extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+}
